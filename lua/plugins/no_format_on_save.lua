39595bb689dea61cd1e4bfb77c273b40e0455e83
petermeng <petermeng_01@163.com>
2026-09-23 14:53:29 +0800

    更新 no_format_on_save.lua

--- lua/plugins/no_format_on_save.lua
+++ lua/plugins/no_format_on_save.lua
@@ -2,5 +2,5 @@
 return {
   "AstroNvim/astrolsp",
   ---@type AstroLSPOpts
-  opts = { formatting = { format_on_save = { enabled = false} } },
+  opts = { formatting = { format_on_save = { enabled = false } } },
 }
